5b04f8aa06f827694bed95b721cf6e6ef2544ae8
hala-ux <58296783+hala-ux@users.noreply.github.com>
2021-07-22 00:12:39 +0300

    Update footer.php

--- wp-content/themes/createtheme/footer.php
+++ wp-content/themes/createtheme/footer.php
@@ -1,10 +1,369 @@
 
-<footer>
-  <div class="jumbotron text-center">
- <h7> All copyrights reserved </h7>
+
+
  
+<section class="">
+  <!-- Footer -->
+  <footer class="bg-dark text-center text-white pt-5">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row">
+       
+
+        <!--Grid column-->
+        <div class="col-lg-3 text-left">
+          <h6 class="text-uppercase mb-3">About Us</h6>
+<p class="text-secondary">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere quos reiciendis <br> <br>recusandae porro nesciunt! Modi dicta perferendis ipsum. Nobis consectetur dolorum neque!</p>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h6 class="text-uppercase mb-3">CONTACT US</h6>
+<h3 class="mb-3"><strong > (800) 123-4567</strong></h3>
+<p class="text-secondary mb-0">International: (333) 456-6670</p>
+<p class="text-secondary mb-0">Fax: (222) 531-8999</p>
+<p class="text-secondary"><i class="fa fa-dot-circle-o pr-3" aria-hidden="true"></i>234 Street Name, City Name</p>
+<i class="fa fa-envelope pr-3" aria-hidden="true"></i><a  class="text-secondary" href="mailto:some@email.com" target="_blank" >mail@example.com</a>
+
+<br>
+
+
 </div>
+        <!--Grid column-->
+         <!--Grid column-->
+         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+          <h5 class="text-uppercase">SEND US A MESSAGE</h5>
+
+          <form action="">
+        <!--Grid row-->
+        <div class="row d-flex justify-content-center">
+         
+
+          <!--Grid column-->
+          <div class="col-md-10 col-12">
+            <!-- Email input -->
+            <div class="form-outline form-white mb-4">
+              <input type="name" id="form5Example2" class="form-control" placeholder="your name..." />
+             
+            </div>
+            <div class="form-outline form-white mb-4">
+              <input type="email" id="form5Example2" class="form-control" placeholder="your email..."  />
+           
+            </div>
+            <div class="form-outline form-white mb-4">
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"placeholder="your message..."></textarea>
+           
+            </div>
+          </div>
+          <!--Grid column-->
+
+         
+        </div>
+        <!--Grid row-->
+         <!--Grid column-->
+         <div class="col-auto">
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-outline-danger pl-4 pr-4">
+              Submit
+            </button>
+          </div>
+          <!--Grid column-->
+      </form>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</section>
+
+<br><br>
+
+<footer class="bg-dark text-white text-lg-start">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h4 >Contact Details</h4>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+            <p class="text-secondary mb-0" >porto template 123</p>
+          </li>
+          <li>
+            <p class="text-secondary mb-0" >Porto Blvd, Suite</p>
+          </li>
+          <li>
+            <p class="text-secondary" >New York</p>
+          </li>
+        
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-3">Opening Hours</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <span href="#!" class="text-secondary">Mon-Fri:</span><span href="#!" class="text-white pl-2">8:30 am to 5:00 pm</span>
+          </li>
+          <li>
+          <span href="#!" class="text-secondary">Mon-Fri:</span><span href="#!" class="text-white pl-2">8:30 am to 5:00 pm</span>
+          </li>
+          <li>
+          <span href="#!" class="text-secondary">Mon-Fri:</span><span href="#!" class="text-white pl-2">8:30 am to 5:00 pm</span>
+          </li>
+          
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-3">Call Us Now</h5>
+
+        <ul class="list-unstyled mb-0">
+          <li>
+          <h4>  <strong href="#!" class="text-white">(800) 123 4567</strong></h4>
+          </li>
+          <li>
+          <span href="#!" class="text-secondary">Sales: </span><span href="#!" class="text-white pl-2">(800) 123 4568</span>
+          </li>
+         
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-3">Social Media</h5>
+
+        <ul class="list-unstyled">
+          <li>
+         <!-- Facebook -->
+      <a class="btn m-1" href="#!" role="button"
+        ><i class="fa fa-facebook-official" style="font-size:24px"></i><i class="fa fa-linkedin ml-3" style="font-size:24px"></i><i class="fa fa-twitter-square ml-3" style="font-size:24px"></i></a>
+          </li>
+      
+          <li>
+            <a href="#!" class="text-dark">Link 3</a>
+          </li>
+          <li>
+            <a href="#!" class="text-dark">Link 4</a>
+          </li>
+        </ul>
+      </div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
 </footer>
+<br><br>
+<!-- Footer -->
+<footer class="bg-dark text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fa fa-facebook-official" style="font-size:24px"></i></a>
+
+      <!--github-->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fa fa-github-square" style="font-size:24px"></i></a>
+
+      <!-- Google -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fa fa-google" style="font-size:24px"></i></a>
+
+      <!-- Instagram -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fa fa-instagram" style="font-size:24px"></i></a>
+
+      <!-- Linkedin -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fa fa-linkedin-square" style="font-size:24px"></i></a>
+
+      <!-- twitter -->
+      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+        ><i class="fa fa-twitter-square" style="font-size:24px"></i></a>
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Form -->
+    <section class="">
+      <form action="">
+        <!--Grid row-->
+        <div class="row d-flex justify-content-center">
+          <!--Grid column-->
+          <div class="col-auto">
+            <p class="pt-2">
+              <strong>Sign up for our newsletter</strong>
+            </p>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-md-5 col-12">
+            <!-- Email input -->
+            <div class="form-outline form-white mb-4">
+              <input type="email" id="form5Example2" class="form-control" />
+              <label class="form-label" for="form5Example2">Email address</label>
+            </div>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-auto">
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-outline-light mb-4">
+              Subscribe
+            </button>
+          </div>
+          <!--Grid column-->
+        </div>
+        <!--Grid row-->
+      </form>
+    </section>
+    <!-- Section: Form -->
+
+    <!-- Section: Text -->
+    <section class="mb-4">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt distinctio earum
+        repellat quaerat voluptatibus placeat nam, commodi optio pariatur est quia magnam
+        eum harum corrupti dicta, aliquam sequi voluptate quas.
+      </p>
+    </section>
+    <!-- Section: Text -->
+
+    <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-white">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-white">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-white">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Links</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-white">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
  <?php
 wp_footer();?>
 </body>
