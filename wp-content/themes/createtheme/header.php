68c48c01dd5edffcd0ecf9728853145f52322eab
hala <halaskhaya@gmail.com>
2021-07-22 13:17:11 +0300

    header change bootstrap

--- wp-content/themes/createtheme/header.php
+++ wp-content/themes/createtheme/header.php
@@ -4,7 +4,8 @@
   <title>Create Thneme</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
