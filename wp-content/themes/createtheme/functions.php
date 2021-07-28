e66c5cae7837fa8c43ca78fcd432b7d13a81e515
boshraa <boshraayoune@gmail.com>
2021-07-27 19:51:09 -0700

    update sidebar 3

--- wp-content/themes/createtheme/functions.php
+++ wp-content/themes/createtheme/functions.php
@@ -3,7 +3,12 @@
 function add_theme_scripts() {
    
     wp_enqueue_style ( 'custom', get_template_directory_uri () . '/assets/css/custom.css', array() ,'1.0.0', 'all');
+    wp_enqueue_style ( 'sidebar', get_template_directory_uri () . '/assets/css/sidebar.css', array() ,'1.0.1', 'all');
 
     wp_enqueue_script( 'custom', get_template_directory_uri() . '/assets/js/custom.js', array ( 'jquery' ), 1.1, true);
+
+    wp_enqueue_script( 'bootstrap.min', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array ( 'jquery' ), 1.1, true);
+     
+     
 }
 add_action ( 'wp_enqueue_scripts', 'add_theme_scripts' );
--- wp-content/themes/createtheme/home.php
+++ wp-content/themes/createtheme/home.php
@@ -1,22 +1,8 @@
 <?php get_header();?>
-
+<?php get_sidebar();?>
 <div class="container">
-  <div class="row">
-    <?php
-if ( have_posts() ) ?>
-   <?php while ( have_posts() ): ?>
-   <?php  the_post();?>
-    <div class="col-sm-4">
-      <h3><?php  the_title();?></h3>
-      <p><?php  the_content();?></p>
-      <?php  the_author();?>
-      <?php  the_time();?>
-    </div>
-   
-    <?php endwhile; ?>
-    
-  </div>
+ 
 </div>
-  
+
 <?php get_footer(); ?>
 
