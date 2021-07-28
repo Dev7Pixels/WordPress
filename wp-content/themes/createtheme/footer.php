8ddf4a160a655da7a8fe2bae8f59ffaae2875a91
hala-ux <58296783+hala-ux@users.noreply.github.com>
2021-07-28 14:07:29 +0300

    add bootstrap rtl link

--- wp-content/themes/createtheme/footer.php
+++ wp-content/themes/createtheme/footer.php
@@ -1,6 +1,7 @@
 
 
 <?php if(is_rtl()){ ?>
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
 <html lang="ar" dir="rtl" >
 
 
@@ -144,10 +145,10 @@
           
           <div class="row"><hr></div>
          <div class="payment-accounts ">
-          <i class="fab fa-cc-visa fa-3x p-1"></i>
-         <i class="fab fa-cc-paypal  fa-3x p-1"></i>
-         <i class="fab fa-cc-stripe fa-3x p-1"></i> 
-         <i class="fab fa-alipay fa-3x p-1"></i>
+         <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/paypal-logo.png"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/Visa1.jpg"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/payments_12_550.png"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/images.png"  alt="..." width="50" height="30"></a>
 <span class="payment-icon visa "></span>
 
          </div>
