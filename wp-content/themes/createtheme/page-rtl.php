0dd7e165a03de9077703a8352361e7e16a48342b
hala-ux <58296783+hala-ux@users.noreply.github.com>
2021-08-03 21:59:15 +0300

    Create page-rtl.php

---
+++ wp-content/themes/createtheme/page-rtl.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous">
+<html lang="ar" dir="rtl" >
+    
+<?php get_header();
+
+?>
+
+
+<div class="container">
+ 
+  <div class="row">
+    <?php
+if ( have_posts() ) ?>
+   <?php while ( have_posts() ): ?>
+   <?php  the_post();?>
+    <div class="col-sm-4">
+      <h3><?php  the_title();?></h3>
+      <p><?php  the_content();?></p>
+      <?php  the_author();?>
+      <?php  the_time();?>
+    </div>
+   
+    <?php endwhile; ?>
+    
+  </div>
+</div>
+  
+<?php get_footer(); ?>
+</html>
