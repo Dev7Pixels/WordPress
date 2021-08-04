892293a047de504e8a83fc7372c80b37e825c3f5
hala-ux <58296783+hala-ux@users.noreply.github.com>
2021-08-04 08:25:24 +0300

    Update functions.php

--- wp-content/themes/createtheme/functions.php
+++ wp-content/themes/createtheme/functions.php
@@ -7,3 +7,18 @@ function add_theme_scripts() {
     wp_enqueue_script( 'custom', get_template_directory_uri() . '/assets/js/custom.js', array ( 'jquery' ), 1.1, true);
 }
 add_action ( 'wp_enqueue_scripts', 'add_theme_scripts' );
+
+
+// if ( is_page_template( 'template-part/page_rtl.php' ) ) {
+//     include_once 'template-part/page_rtl.php';
+// }
+
+function register_my_menus() {
+    register_nav_menus(
+      array(
+        'header-menu' => __( 'Header Menu' ),
+        'menu_class'     =>  __( 'navigation-main' ),
+      )
+    );
+  }
+  add_action( 'init', 'register_my_menus' );
