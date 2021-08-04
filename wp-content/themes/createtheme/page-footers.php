d06f7752c982748f3d1ef3596ad5d684757c2e61
hala-ux <58296783+hala-ux@users.noreply.github.com>
2021-08-04 13:13:16 +0300

    Create page-footers.php

---
+++ wp-content/themes/createtheme/page-footers.php
@@ -0,0 +1,426 @@
+<!-- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<section class="">
+  <!-- Footer -->
+  <footer class="bg-dark text-center text-white pt-5">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row">
+       
+
+        <!--Grid column-->
+        <div class="col-lg-3 text-left">
+          <h6 class="text-uppercase mb-3">About Us</h6>
+<p class="text-secondary">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere quos reiciendis <br> <br>recusandae porro nesciunt! Modi dicta perferendis ipsum. Nobis consectetur dolorum neque!</p>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h6 class="text-uppercase mb-3">CONTACT US</h6>
+<h3 class="mb-3"><strong > (800) 123-4567</strong></h3>
+<p class="text-secondary mb-0">International: (333) 456-6670</p>
+<p class="text-secondary mb-0">Fax: (222) 531-8999</p>
+<p class="text-secondary"><i class="fa fa-dot-circle-o pr-3" aria-hidden="true"></i>234 Street Name, City Name</p>
+<i class="fa fa-envelope pr-3" aria-hidden="true"></i><a  class="text-secondary" href="mailto:some@email.com" target="_blank" >mail@example.com</a>
+
+<br>
+
+
+</div>
+        <!--Grid column-->
+         <!--Grid column-->
+         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+          <h5 class="text-uppercase">SEND US A MESSAGE</h5>
+
+          <form action="">
+        <!--Grid row-->
+        <div class="row d-flex justify-content-center">
+         
+
+          <!--Grid column-->
+          <div class="col-md-10 col-12">
+            <!-- Email input -->
+            <div class="form-outline form-white mb-4">
+              <input type="name" id="form5Example2" class="form-control" placeholder="your name..." />
+             
+            </div>
+            <div class="form-outline form-white mb-4">
+              <input type="email" id="form5Example2" class="form-control" placeholder="your email..."  />
+           
+            </div>
+            <div class="form-outline form-white mb-4">
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"placeholder="your message..."></textarea>
+           
+            </div>
+          </div>
+          <!--Grid column-->
+
+         
+        </div>
+        <!--Grid row-->
+         <!--Grid column-->
+         <div class="col-auto">
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-outline-danger pl-4 pr-4">
+              Submit
+            </button>
+          </div>
+          <!--Grid column-->
+      </form>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+   
+  <!-- Copyright -->
+  <div class="" style="background-color: rgba(0, 0, 0, 0.2);">
+  <div class="row">
+  <div class="col-lg-3 fs-6 text-secondary m-2 text-center">
+  © 2020 Copyright:All Right Preserved
+  </div>
+  <div class="col-lg-5 text-center">
+  <div class="payment-accounts p-2 ">
+     
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/paypal-logo.png"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/Visa1.jpg"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/payments_12_550.png"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/images.png"  alt="..." width="50" height="30"></a>
+
+
+         </div>
+  </div>
+    <div class="col-lg-3 text-center">
+      <a href="#"><i class="fa fa-facebook-f p-2 m-2" style="color:white;background-color:#33332F;border-radius:3px;"></i></a>
+      <a href="#"><i class="fab fa-twitter  fa-1x p-2 m-2" style="color:white;background-color:#33332F;border-radius:3px;"></i></a>
+      <a href="#"><i class="fa fa-linkedin fa-1x p-2 m-2" style="color:white;background-color:#33332F;border-radius:3px;"></i></a>
+      
+    </div>
+</div>
+</div>
+  <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</section>
+
+<br><br>
+
+<footer class="bg-dark text-white">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!--Grid row-->
+    <div class="row">
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0 ">
+        <h4 >Contact Details</h4>
+
+        <ul class="list-unstyled p-0 m-0 mb-0">
+          <li class="">
+            <p class="text-secondary mb-0" >porto template 123</p>
+          </li>
+          <li>
+            <p class="text-secondary  mb-0" >Porto Blvd, Suite</p>
+          </li>
+          <li>
+            <p class="text-secondary" >New York</p>
+          </li>
+        
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-3">Opening Hours</h5>
+
+        <ul class="list-unstyled p-0 m-0">
+          <li>
+            <span href="#!" class="text-secondary">Mon-Fri:</span><span href="#!" class="text-white pl-2">8:30 am to 5:00 pm</span>
+          </li>
+          <li>
+          <span href="#!" class="text-secondary">Mon-Fri:</span><span href="#!" class="text-white pl-2">8:30 am to 5:00 pm</span>
+          </li>
+          <li>
+          <span href="#!" class="text-secondary">Mon-Fri:</span><span href="#!" class="text-white pl-2">8:30 am to 5:00 pm</span>
+          </li>
+          
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-3">Call Us Now</h5>
+
+        <ul class="list-unstyled p-0 m-0">
+          <li>
+          <h4>  <strong href="#!" class="text-white">(800) 123 4567</strong></h4>
+          </li>
+          <li>
+          <span href="#!" class="text-secondary">Sales: </span><span href="#!" class="text-white pl-2">(800) 123 4568</span>
+          </li>
+         
+        </ul>
+      </div>
+      <!--Grid column-->
+
+      <!--Grid column-->
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase mb-3">Social Media</h5>
+<div class="">
+        <a href="#"><i class="fa fa-facebook-f p-1" style="padding:5px;color:white;background-color:#33332F;border-radius:3px;"></i></a>
+<a href="#"><i class="fab fa-git  fa-1x p-1" style="padding:5px;color:white;background-color:#33332F;border-radius:3px;"></i></a>
+<a href="#"><i class="fa fa-linkedin fa-1x p-1" style="padding:3px;color:white;background-color:#33332F;border-radius:3px;"></i></a>
+      </div></div>
+      <!--Grid column-->
+    </div>
+    <!--Grid row-->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="" style="background-color: rgba(0, 0, 0, 0.2);">
+  <div class="row">
+  <div class="col-lg-3 fs-6 text-secondary m-2 text-center">
+  © 2020 Copyright:All Right Preserved
+  </div>
+  <div class="col-lg-5 text-center">
+  <div class="payment-accounts p-2 ">
+     
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/paypal-logo.png"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/Visa1.jpg"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/payments_12_550.png"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/images.png"  alt="..." width="50" height="30"></a>
+
+
+         </div>
+  </div>
+    <div class="col-lg-3 text-center">
+      <a href="#"><i class="fa fa-facebook-f p-2 m-2" style="color:white;background-color:#33332F;border-radius:3px;"></i></a>
+      <a href="#"><i class="fab fa-twitter  fa-1x p-2 m-2" style="color:white;background-color:#33332F;border-radius:3px;"></i></a>
+      <a href="#"><i class="fa fa-linkedin fa-1x p-2 m-2" style="color:white;background-color:#33332F;border-radius:3px;"></i></a>
+      
+    </div>
+</div>
+</div>
+  <!-- Copyright -->
+</footer>
+<br><br>
+<!-- Footer -->
+
+<footer class="bg-dark text-white pt-5">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Social media -->
+   
+    <!-- Section: Social media -->
+
+    <!-- Section: Form -->
+    <section class="">
+      <form action="">
+        <!--Grid row-->
+       
+          <!--Grid column-->
+          
+          <!--Grid column-->
+<div class="row">
+<!--Grid column-->
+<div class="col-lg-5 col-md-6 mb-4 pr-4 mb-md-4 float-start ">
+<h5 >Be The First To Know</h5>
+<p class="text-secondary"> get at the latest internationality get at the latest internationality  </p>
+</div>
+<!--Grid column-->
+
+
+          <!--Grid column-->
+          <div class="col-lg-4 col-md-6 mb-4 mb-md-0  ">
+<div class="input-group mb-3 ">
+  <input type="text" class="form-control border-top rounded-pill" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+  <div class="input-group-append">
+    <button class="btn btn-primary rounded-pill" type="button">Subscribe</button>
+  </div>
+</div> </div>
+<div class="col-lg-3 col-md-2 mb-4 mb-md-0 text-center">
+
+<a href="#"><i class="fa fa-facebook-f p-1" style="padding:5px;color:white;background-color:#33332F;border-radius:3px;"></i></a>
+<a href="#"><i class="fab fa-git  fa-1x p-1" style="padding:5px;color:white;background-color:#33332F;border-radius:3px;"></i></a>
+<a href="#"><i class="fa fa-linkedin fa-1x p-1" style="padding:5px;color:white;background-color:#33332F;border-radius:3px;"></i></a>
+</div>
+</div><!--end row-->
+          <!--Grid column-->
+
+          <!--Grid column-->
+       
+        <!--Grid row-->
+      </form>
+    </section>
+    <!-- Section: Form -->
+
+    <!-- Section: Text -->
+    <section class="mb-4">
+     <hr>
+    </section>
+    <!-- Section: Text -->
+
+    <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+          <h6 class="text-uppercase">contact information</h6>
+
+         
+              
+
+          <span class="text-white text-uppercase pb-0 pt-2">ADDRESS</span></span><br>
+              <span class="text-secondary pt-0">123STREET</span>
+              <br>
+             
+         
+            <span class="text-white text-uppercase pb-0">phone</span></span><br>
+              <span class="text-secondary pt-0">125 -445 -777</span>
+           
+           <br>
+            <span class="text-white text-uppercase pb-0">email</span></span><br>
+              <span class="text-secondary pt-0">hala@gmail.com</span>
+            
+           
+         
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">my account</h5>
+
+          <ul class="list-unstyled mb-3">
+            <li>
+              <a href="#!" class="text-secondary text-decoration-none">About Us</a>
+            </li>
+            <li>
+              <a href="#!" class="text-secondary text-decoration-none">Contact Us</a>
+            </li>
+            <li>
+              <a href="#!" class="text-secondary text-decoration-none">My Account</a>
+            </li>
+           
+          </ul>
+          <div class="row pt-1"><hr></div>
+         @porto commerce
+        </div>
+        
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase pb-4">  </h5>
+
+          <ul class="list-unstyled ">
+            <li>
+              <a href="#!" class="text-secondary text-decoration-none">order history</a>
+            </li>
+            <li>
+              <a href="#!" class="text-secondary text-decoration-none">Search</a>
+            </li>
+            <li>
+              <a href="#!" class="text-secondary text-decoration-none">login</a>
+            </li>
+        
+          </ul>
+          
+          <div class="row pt-1"><hr></div>
+          2019 copy right
+        </div>
+        <!--Grid column-->
+        
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase pb-4">Main Feature</h5>
+
+          <p class="text-secondary"> super and fast wordpress theme with many fantastic features</p>
+         
+          <div class="row pt-1"><hr></div>
+          working days hour <br> <p class="text-secondary">from10 to 4</p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase pb-5"></h5>
+
+          <p class="text-secondary"> super and fast wordpress theme with many fantastic features</p>
+          
+          <div class="row pt-1"><hr></div>
+         <div class="payment-accounts ">
+         <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/paypal-logo.png"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/Visa1.jpg"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/payments_12_550.png"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/images.png"  alt="..." width="50" height="30"></a>
+<span class="payment-icon visa "></span>
+
+         </div>
+        </div>
+        <!--Grid column-->
+        <!--row-->
+      
+      </div>
+      </span>
+      
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="" style="background-color: rgba(0, 0, 0, 0.2);">
+  <div class="row">
+  <div class="col-lg-3 fs-6 text-secondary m-2 text-center">
+  © 2020 Copyright:All Right Preserved
+  </div>
+  <div class="col-lg-5 text-center">
+  <div class="payment-accounts p-2 ">
+     
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/paypal-logo.png"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/Visa1.jpg"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/payments_12_550.png"  alt="..." width="50" height="30"></a>
+      <a href="#"> <img src="http://localhost/wordpress/wp-content/uploads/2021/07/images.png"  alt="..." width="50" height="30"></a>
+
+
+         </div>
+  </div>
+    <div class="col-lg-3 text-center">
+      <a href="#"><i class="fa fa-facebook-f p-2 m-2" style="color:white;background-color:#33332F;border-radius:3px;"></i></a>
+      <a href="#"><i class="fab fa-twitter  fa-1x p-2 m-2" style="color:white;background-color:#33332F;border-radius:3px;"></i></a>
+      <a href="#"><i class="fa fa-linkedin fa-1x p-2 m-2" style="color:white;background-color:#33332F;border-radius:3px;"></i></a>
+      
+    </div>
+</div>
+</div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+
+
+ 
+</body>
+</html>
+  
+
