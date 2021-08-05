23f33a01d6127a4edcec701a880c53563fdc489e
hala-ux <58296783+hala-ux@users.noreply.github.com>
2021-08-05 12:41:05 +0300

    Create page-about-us.php

---
+++ wp-content/themes/createtheme/page-about-us.php
@@ -0,0 +1,172 @@
+<?php
+
+/*
+Template name:about-us
+
+*/
+
+
+?>
+<?php get_header();?>
+<!-- Section About Us -->
+<section id="banner">
+    <div id="carouselExampleCaptions" >
+        
+        <div class="carousel-inner">
+            
+            <div class="carousel-item active">
+                <img src="https://www.markuptag.com/images/slider-img-3.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h1 class="mb-5 ">About Us</h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
+                    <a href="#" class="btn btn-warning">Read More</a>
+                </div>
+            </div>
+        </div>
+       
+        
+    </div>
+</section>
+<!-- End Banner Section -->
+
+
+<!-- Hero Banner Section -->
+<section class="hero-banner bg-light py-5">
+    <div class="container">
+        <div class="row row align-items-center">
+            <div class="col-lg-5 offset-lg-1 order-lg-1">
+                <img style="width: 100%; height: 30rem;" src="<?php bloginfo('template_url'); ?>/assets/img/home-bg.jpg" class="img-fluid" alt="Web Development">
+            </div>
+            <div class="col-lg-6">
+                <h2 class="mt-3"> We Are Web Designing & Development</h2>
+                <p class="lead text-secondary my-5">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section>
+  
+  <div class="container shadow mb-5 mt-5">
+    <div class="row">
+        <div class="col-12 p-0">
+            <h1 class="text-center border-bottom mb-0 p-4">Our Team</h1>
+        </div>
+        <div class="col-sm-6 border-end border-bottom p-4">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="https://i.imgur.com/J6l19aF.jpg" class="img-thumbnail rounded-circle" alt="User">
+                </div>
+                <div class="col-md-9">
+                    <figure>
+                        <blockquote class="blockquote mt-3">
+                            <p class="fs-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum the industry's standard dummy text</p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer">John Son</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 border-bottom p-4">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="https://i.imgur.com/Ur43esv.jpg" class="img-thumbnail rounded-circle" alt="User">
+                </div>
+                <div class="col-md-9">
+                    <figure>
+                        <blockquote class="blockquote mt-3">
+                            <p class="fs-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum the industry's standard dummy text</p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer">John Son</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 border-end p-4">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="https://i.imgur.com/Ur43esv.jpg" class="img-thumbnail rounded-circle" alt="User">
+                </div>
+                <div class="col-md-9">
+                    <figure>
+                        <blockquote class="blockquote mt-3">
+                            <p class="fs-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum the industry's standard dummy text</p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer">John Son</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 p-4">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="https://i.imgur.com/8RKXAIV.jpg" class="img-thumbnail rounded-circle" alt="User">
+                </div>
+                <div class="col-md-9">
+                    <figure>
+                        <blockquote class="blockquote mt-3">
+                            <p class="fs-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum the industry's standard dummy text</p>
+                        </blockquote>
+                        <figcaption class="blockquote-footer">John Son</figcaption>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+<footer id="footer">
+    <div class="footer-newsletter">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <h4>Our Newsletter</h4>
+                    <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+                </div>
+                <div class="col-lg-6">
+                    <form action="" method="post"> <input type="email" name="email"><input type="submit" value="Subscribe"> </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Our Services</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h4>Contact Us</h4>
+                    <p> A108 Adam Street <br> New York, NY 535022<br> United States <br><br> <strong>Phone:</strong> +1 5589 55488 55<br> <strong>Email:</strong> info@example.com<br> </p>
+                </div>
+                <div class="col-lg-3 col-md-6 footer-info">
+                    <h3>About DevVE</h3>
+                    <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+   
+</footer>
+
